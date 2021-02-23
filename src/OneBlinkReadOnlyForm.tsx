5c82079ffecb0ae594eb2f1ec41013da6105cc44
David Porter <david@oneblink.io>
2021-02-23 16:28:49 +1100

    ON-10219 # set all elements inside a OneBlinkReadOnlyForm to readOnly

--- src/OneBlinkReadOnlyForm.tsx
+++ src/OneBlinkReadOnlyForm.tsx
@@ -4,11 +4,29 @@ import { FormTypes } from '@oneblink/types'
 
 import OneBlinkFormBase from './OneBlinkFormBase'
 
+import _cloneDeep from 'lodash.clonedeep'
+
+function recursivelySetReadOnly(formElements: FormTypes.FormElement[]) {
+  const newFormElements = formElements.map((element) => {
+    if (
+      (element.type === 'form' || element.type === 'infoPage') &&
+      Array.isArray(element.elements)
+    ) {
+      element.elements = recursivelySetReadOnly(element.elements)
+    }
+    if ('readOnly' in element) {
+      element.readOnly = true
+    }
+    return element
+  })
+  return newFormElements
+}
+
 type Props = {
   form: FormTypes.Form
   initialSubmission?: FormElementsCtrl['model'] | null
   googleMapsApiKey?: string
-  onFormError:() => unknown
+  onFormError: () => unknown
 }
 
 function OneBlinkFormReadOnly({
@@ -17,10 +35,19 @@ function OneBlinkFormReadOnly({
   initialSubmission,
   onFormError,
 }: Props) {
+  const [definition, setDefinition] = React.useState<FormTypes.Form>(() =>
+    _cloneDeep(form),
+  )
+
+  React.useCallback(() => {
+    const newElements = recursivelySetReadOnly(form.elements)
+    const newDefinition = { ...form, elements: newElements }
+    setDefinition(newDefinition)
+  }, [form])
 
   return (
     <OneBlinkFormBase
-      form={form}
+      form={definition}
       disabled={true}
       googleMapsApiKey={googleMapsApiKey}
       initialSubmission={initialSubmission}
