0ca0abf7c1876c0908197ec677155649e634fdda
Matt Carroll <matt.carroll139@gmail.com>
2021-04-22 15:01:23 +1000

    ON-11472 # Added zoom property to location form elements submission data

--- src/form-elements/FormElementLocation.tsx
+++ src/form-elements/FormElementLocation.tsx
@@ -26,6 +26,24 @@ type Props = {
 type Coords = {
   latitude: number
   longitude: number
+  zoom: number
+}
+
+function parseLocationValue(value: unknown): Coords | undefined {
+  if (!value || typeof value !== 'object') {
+    return
+  }
+
+  const { latitude, longitude, zoom } = value as Record<string, unknown>
+  if (typeof latitude !== 'number' || typeof longitude !== 'number') {
+    return
+  }
+
+  return {
+    latitude,
+    longitude,
+    zoom: typeof zoom === 'number' ? zoom : initialMapZoom,
+  }
 }
 
 const mapHeight = 300
@@ -39,26 +57,13 @@ function FormElementLocation({
   validationMessage,
   displayValidationMessage,
 }: Props) {
-  const isOffline = useIsOffline()
   const [isDirty, setIsDirty] = useBooleanState(false)
   const [
-    isLoadingLocation,
-    setIsLoadingLocation,
-    setIsNotLoadingLocation,
-  ] = useBooleanState(false)
-  const [
-    locationPickerIsOpen,
+    isLocationPickerOpen,
     showLocationPicker,
     hideLocationPicker,
   ] = useBooleanState(false)
 
-  const setCurrentLocation = React.useCallback(async () => {
-    const { coords } = await geolocation.getCurrentPosition()
-    if (coords) {
-      const { latitude = 0, longitude = 0 } = coords
-      setLocation({ latitude, longitude })
-    }
-  }, [])
   const [location, setLocation] = React.useState<Coords | undefined>(undefined)
   const onClear = React.useCallback(() => {
     hideLocationPicker()
@@ -68,29 +73,32 @@ function FormElementLocation({
 
   const onCancel = React.useCallback(() => {
     hideLocationPicker()
-    setLocation(value as Coords | undefined)
+    setLocation(parseLocationValue(value))
   }, [hideLocationPicker, value])
 
   const onLocate = React.useCallback(async () => {
-    setIsLoadingLocation()
-    try {
-      if (!location) {
-        await setCurrentLocation()
-      }
-    } catch (err) {
-      Sentry.captureException(err)
-      console.error(err)
-      setLocation(defaultCoords())
-    }
     showLocationPicker()
-    setIsNotLoadingLocation()
-  }, [
-    location,
-    setCurrentLocation,
-    showLocationPicker,
-    setIsLoadingLocation,
-    setIsNotLoadingLocation,
-  ])
+
+    if (location) {
+      return
+    }
+
+    let currentLocation = null
+    try {
+      const result = await geolocation.getCurrentPosition()
+      currentLocation = result.coords
+    } catch (err) {
+      console.error(
+        'Error while attempting to find the users current location',
+        err,
+      )
+      Sentry.captureException(err)
+    } finally {
+      setLocation(parseLocationValue(currentLocation || defaultCoords()))
+    }
+  }, [location, showLocationPicker])
+
+  const isLoadingLocation = !location && isLocationPickerOpen
 
   const onConfirm = React.useCallback(() => {
     setIsDirty()
@@ -100,10 +108,12 @@ function FormElementLocation({
 
   // SET DEFAULT/PREFILL DATA
   React.useEffect(() => {
-    if (value) {
-      setLocation(value as Coords | undefined)
+    const newValue = parseLocationValue(value)
+    if (newValue) {
+      setLocation(newValue)
     }
   }, [value])
+
   return (
     <div className="cypress-location-element">
       <FormElementLabelContainer
@@ -113,28 +123,15 @@ function FormElementLocation({
         required={element.required}
       >
         <div className="control">
-          {!isOffline ? (
-            <LocationPicker
-              isOpen={locationPickerIsOpen}
-              value={location}
-              onChange={setLocation}
-            ></LocationPicker>
-          ) : (
-            <LocationIsOffline
-              isOpen={locationPickerIsOpen}
-              location={location}
-            ></LocationIsOffline>
-          )}
-          {isLoadingLocation && (
-            <figure className="ob-figure">
-              <div className="figure-content">
-                <OnLoading small></OnLoading>
-              </div>
-            </figure>
-          )}
+          <LocationDisplay
+            isOpen={isLocationPickerOpen}
+            isLoading={isLoadingLocation}
+            location={location}
+            onChange={setLocation}
+          />
 
           <div className="buttons ob-buttons ob-location__buttons">
-            {locationPickerIsOpen ? (
+            {isLocationPickerOpen ? (
               <>
                 <button
                   type="button"
@@ -188,6 +185,69 @@ function FormElementLocation({
   )
 }
 
+const LocationDisplay = React.memo(function LocationDisplay({
+  isLoading,
+  isOpen,
+  location,
+  onChange,
+}: {
+  isLoading: boolean
+  isOpen: boolean
+  location: Coords | undefined
+  onChange: (location: Coords) => void
+}) {
+  const isOffline = useIsOffline()
+
+  if (isLoading) {
+    return (
+      <figure className="ob-figure">
+        <div className="figure-content">
+          <OnLoading small></OnLoading>
+        </div>
+      </figure>
+    )
+  }
+
+  if (isOffline) {
+    return <LocationIsOffline location={location} isOpen={isOpen} />
+  }
+
+  if (isOpen) {
+    if (!location) {
+      // There is no location to display while user is attempting to pick a location.
+      // This should never happen, if loading has finished a default should be set.
+      // Fail fast!!!
+      // https://en.wikipedia.org/wiki/Fail-fast
+      throw new Error(
+        'Default location was not set for "location" form element',
+      )
+    }
+
+    return <LocationPicker location={location} onChange={onChange} />
+  }
+
+  if (location) {
+    return <LocationImage location={location} />
+  }
+
+  return null
+})
+
+function LocationIsOfflineFigureContent({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <figure className="ob-figure">
+      <div className="figure-content has-text-centered">
+        <h4 className="title is-4">You are currently offline</h4>
+        {children}
+      </div>
+    </figure>
+  )
+}
+
 const LocationIsOffline = React.memo(function LocationIsOffline({
   location,
   isOpen,
@@ -195,127 +255,143 @@ const LocationIsOffline = React.memo(function LocationIsOffline({
   location: Coords | undefined
   isOpen: boolean
 }) {
+  if (isOpen) {
+    if (location) {
+      // If user is offline and attempting to pick a location and there is one set already
+      return (
+        <LocationIsOfflineFigureContent>
+          <p>
+            Click the <b>Confirm</b> button below to set the location to your
+            current position.
+          </p>
+        </LocationIsOfflineFigureContent>
+      )
+    }
+
+    // If user is offline and attempting to pick a location and there is one nothing set yet
+    return (
+      <LocationIsOfflineFigureContent>
+        <p>
+          We could not find your current location. Click the <b>Cancel</b>{' '}
+          button below to try again.
+        </p>
+      </LocationIsOfflineFigureContent>
+    )
+  }
+
+  // If user is offline and has confirmed a location
+  if (location) {
+    return (
+      <LocationIsOfflineFigureContent>
+        <h3 className="title is-3 ob-location__latitude">Latitude</h3>
+        <p>
+          <b>{location.latitude}</b>
+        </p>
+        <h3 className="title is-3 ob-location__longitude">Longitude</h3>
+        <p>
+          <b>{location.longitude}</b>
+        </p>
+      </LocationIsOfflineFigureContent>
+    )
+  }
+
+  // User is offline with no location set and not attempting to pick a location
+  return null
+})
+
+const LocationImage = React.memo(function LocationImage({
+  location,
+}: {
+  location: Coords
+}) {
+  const googleMapsApiKey = useGoogleMapsApiKeyKey()
+
+  const staticUrl = React.useMemo(() => {
+    const center = `${location.latitude},${location.longitude}`
+    const queries = {
+      key: googleMapsApiKey,
+      size: `${mapHeight}x${mapHeight}`,
+      zoom: location.zoom,
+      center,
+      markers: `color:red|${center}`,
+    }
+    return `${apiUrl}?${queryString.stringify(queries)}`
+  }, [googleMapsApiKey, location])
+
   return (
-    <div>
-      {isOpen ? (
-        <figure className="ob-figure">
-          <div className="figure-content has-text-centered">
-            <h4 className="title is-4">You are currently offline</h4>
-            {location ? (
-              <p>
-                Click the <b>Confirm</b> button below to set the location to
-                your current position.
-              </p>
-            ) : (
-              <p>
-                We could not find your current location. Click the <b>Cancel</b>{' '}
-                button below to try again.
-              </p>
-            )}
-          </div>
-        </figure>
-      ) : location ? (
-        <figure className="ob-figure">
-          <div className="figure-content has-text-centered">
-            <h4 className="title is-4">You are currently offline</h4>
-            <h3 className="title is-3 ob-location__latitude">Latitude</h3>
-            <p>
-              <b>{location.latitude}</b>
-            </p>
-            <h3 className="title is-3 ob-location__longitude">Longitude</h3>
-            <p>
-              <b>{location.longitude}</b>
-            </p>
-          </div>
-        </figure>
-      ) : null}
-    </div>
+    <figure className="ob-figure">
+      <img
+        className="ob-location__map"
+        alt={`map with center at ${location.latitude} latitude, ${location.longitude} longitude`}
+        src={staticUrl}
+        height={mapHeight}
+        width={mapHeight}
+      />
+    </figure>
   )
 })
 
 const LocationPicker = React.memo(function SummaryResult({
-  value,
-  isOpen,
+  location,
   onChange,
 }: {
-  value: Coords | undefined
-  isOpen: boolean
-  onChange: (newValue: Coords) => void
+  location: Coords
+  onChange: (newLocation: Coords) => void
 }) {
   const googleMapsApiKey = useGoogleMapsApiKeyKey()
 
-  const coords = React.useMemo(
-    () =>
-      value &&
-      typeof value === 'object' &&
-      typeof value.latitude === 'number' &&
-      typeof value.longitude === 'number'
-        ? {
-            lat: value.latitude,
-            lng: value.longitude,
-          }
-        : undefined,
-    [value],
-  )
-
   const [map, setMap] = React.useState(null)
-  const [zoom, setZoom] = React.useState<number>(initialMapZoom)
-  const onZoomChanged = React.useCallback(() => {
-    if (!map) return
-    // @ts-expect-error ???
-    setZoom(map.getZoom())
-  }, [map])
-  const staticUrl = React.useMemo(() => {
-    if (!coords) return
-    const queries = {
-      key: googleMapsApiKey,
-      size: `${mapHeight}x${mapHeight}`,
-      zoom: zoom,
-      center: `${coords.lat},${coords.lng}`,
-      markers: `color:red|${coords.lat},${coords.lng}`,
-    }
-    return `${apiUrl}?${queryString.stringify(queries)}`
-  }, [coords, googleMapsApiKey, zoom])
 
-  return coords ? (
+  const center = React.useMemo(() => {
+    return {
+      lat: location.latitude,
+      lng: location.longitude,
+    }
+  }, [location.latitude, location.longitude])
+
+  const onZoomChanged = React.useCallback(() => {
+    if (!map) {
+      return
+    }
+
+    onChange({
+      latitude: location.latitude,
+      longitude: location.longitude,
+      // @ts-expect-error ???
+      zoom: map.getZoom(),
+    })
+  }, [location.latitude, location.longitude, map, onChange])
+
+  return (
     <figure className="ob-figure">
-      {isOpen ? (
-        <LoadScript googleMapsApiKey={googleMapsApiKey || ''}>
-          <GoogleMap
-            onLoad={(map) => setMap(map)}
-            onUnmount={() => setMap(null)}
-            mapContainerStyle={{
-              height: 300,
+      <LoadScript googleMapsApiKey={googleMapsApiKey || ''}>
+        <GoogleMap
+          onLoad={(map) => setMap(map)}
+          onUnmount={() => setMap(null)}
+          mapContainerStyle={{
+            height: 300,
+          }}
+          center={center}
+          zoom={location.zoom}
+          onZoomChanged={onZoomChanged}
+        >
+          <Marker
+            animation={2} // DOCUMENTATION IS POOR, THIS IS DROP ANIMATION
+            position={center}
+            draggable
+            onDragEnd={(e) => {
+              const { lat, lng } = e.latLng.toJSON()
+              onChange({
+                latitude: lat,
+                longitude: lng,
+                zoom: location.zoom,
+              })
             }}
-            center={coords}
-            zoom={zoom}
-            onZoomChanged={onZoomChanged}
-          >
-            <Marker
-              animation={2} // DOCUMENTATION IS POOR, THIS IS DROP ANIMATION
-              position={coords}
-              draggable
-              onDragEnd={(e) => {
-                const { lat, lng } = e.latLng.toJSON()
-                onChange({
-                  latitude: lat,
-                  longitude: lng,
-                })
-              }}
-            ></Marker>
-          </GoogleMap>
-        </LoadScript>
-      ) : (
-        <img
-          className="ob-location__map"
-          alt={`map with center at ${coords.lat} latitude, ${coords.lng} longitude`}
-          src={staticUrl}
-          height={mapHeight}
-          width={mapHeight}
-        />
-      )}
+          ></Marker>
+        </GoogleMap>
+      </LoadScript>
     </figure>
-  ) : null
+  )
 })
 
 export default React.memo(FormElementLocation)
