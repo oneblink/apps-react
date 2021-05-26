3cfb4c6049c341bc2866ece9fe738d1e0b119100
Matt Carroll <matt.carroll139@gmail.com>
2021-05-26 16:57:35 +1000

    ON-12319 # Added index replaceable to html form elements

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - validation for `camera` form element to ensure an image is selected
+- replaceable parameter `{INDEX}` in HTML elements nested in repeatable sets
 
 ### Changed
 
--- src/form-elements/FormElementHTML.tsx
+++ src/form-elements/FormElementHTML.tsx
@@ -1,19 +1,25 @@
 import * as React from 'react'
 import sanitizeHtml from '../services/sanitize-html'
 import { FormTypes } from '@oneblink/types'
+import { RepeatableSetIndexContext } from './FormElementRepeatableSet'
 
 type Props = {
   element: FormTypes.HtmlElement
 }
 
 function FormElementHTML({ element }: Props) {
+  const index = React.useContext(RepeatableSetIndexContext)
+  const html = React.useMemo(() => {
+    return element.defaultValue.replace('{INDEX}', (index + 1).toString())
+  }, [element.defaultValue, index])
+
   return (
     <div className="cypress-html-element">
       <div className="ob-form__element ob-information cypress-html-element">
         <div
           className="cypress-html-element-content ob-information__content ql-editor"
           dangerouslySetInnerHTML={{
-            __html: sanitizeHtml(element.defaultValue),
+            __html: sanitizeHtml(html),
           }}
         />
       </div>
