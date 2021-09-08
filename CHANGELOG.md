e08cbeb9ac690f55104ae3f04f65adb4b2055c18
Matt Carroll <matt.carroll139@gmail.com>
2021-09-08 10:45:32 +1000

    ON-14951 # Fixed return type for useNullableState hook

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - `setResult` to result from `userLoadDataState()` hook
 
+### Fixed
+
+- return type for `useNullableState()` hook
+
 ## [0.4.13] - 2021-09-01
 
 ### Dependencies
--- src/hooks/useNullableState.ts
+++ src/hooks/useNullableState.ts
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 export default function useNullableState<T>(
   defaultValue: T | null,
-): [T | null, (value: T) => void, () => void] {
+): [T | null, React.Dispatch<React.SetStateAction<T | null>>, () => void] {
   const [state, setState] = React.useState(defaultValue)
   const unsetState = React.useCallback(() => setState(null), [])
   return [state, setState, unsetState]
