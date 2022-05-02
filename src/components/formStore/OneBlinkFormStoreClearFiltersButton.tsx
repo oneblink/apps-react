dd270cf14772b0bf587842b5c1073004696961a5
Matt Carroll <matt.carroll139@gmail.com>
2022-05-02 15:09:59 +1000

    ON-21010 # Added disabled to clear filters buttons for form store

--- src/components/formStore/OneBlinkFormStoreClearFiltersButton.tsx
+++ src/components/formStore/OneBlinkFormStoreClearFiltersButton.tsx
@@ -6,11 +6,15 @@ import useFormStoreTableContext from './useFormStoreTableContext'
 function OneBlinkFormStoreClearFiltersButton(
   props: React.ComponentProps<typeof Button>,
 ) {
-  const { onChangeFilters } = useFormStoreTableContext()
+  const { onChangeFilters, filters } = useFormStoreTableContext()
+  const isDisabled = React.useMemo(() => {
+    return !Object.keys(filters).some((key) => key !== 'sorting')
+  }, [filters])
   return (
     <Button
       className="ob-form-store-clear-filters-button"
       startIcon={<FilterIcon />}
+      disabled={isDisabled}
       onClick={() => {
         onChangeFilters(
           (currentFilters) => ({
