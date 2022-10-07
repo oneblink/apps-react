f03158a7c450c9dceed7eff8557b1481e8531905
Matt Carroll <matt.carroll139@gmail.com>
2022-10-07 16:46:10 +1100

    Removed CircularProgress

--- src/components/renderer/attachments/AttachmentStatus.tsx
+++ src/components/renderer/attachments/AttachmentStatus.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { CircularProgress, Tooltip } from '@mui/material'
+import { Tooltip } from '@mui/material'
 import useIsOffline from '../../../hooks/useIsOffline'
 
 const AttachmentStatus = ({
