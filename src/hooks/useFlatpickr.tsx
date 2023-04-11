757de4f15a78ce8ca396a2b6f6000ea2fb7f9127
Matt Carroll <matt.carroll139@gmail.com>
2023-04-11 12:27:46 +1000

    ON-30733 # Fixed flatpickr stripping value from submission

--- src/hooks/useFlatpickr.tsx
+++ src/hooks/useFlatpickr.tsx
@@ -7,6 +7,18 @@ import useFlatpickrGuid from '../hooks/useFlatpickrGuid'
 
 export { FlatpickrOptions }
 
+const getDateValue = (
+  vp: FlatpickrInstance,
+  date: Date | undefined,
+  dateOnly: boolean | undefined,
+) => {
+  if (!date) return
+  if (dateOnly) {
+    return vp.formatDate(date, 'Y-m-d')
+  }
+  return date.toISOString()
+}
+
 export default function useFlatpickr(
   {
     id,
@@ -25,17 +37,6 @@ export default function useFlatpickr(
   const flatpickrGuid = useFlatpickrGuid()
   const vpRef = React.useRef<FlatpickrInstance | null>(null)
 
-  const getDateValue = React.useCallback(
-    (date: Date | undefined) => {
-      if (!date) return
-      if (dateOnly) {
-        return vpRef.current?.formatDate(date, 'Y-m-d')
-      }
-      return date.toISOString()
-    },
-    [dateOnly],
-  )
-
   React.useEffect(() => {
     const options: FlatpickrOptions = {
       ...fpOpts,
@@ -46,6 +47,9 @@ export default function useFlatpickr(
       `[id="${flatpickrGuid}"] [id="${id}"]`,
       options,
     )
+    newVp.set('onChange', (selectedDates: Date[]) => {
+      onChange(getDateValue(newVp, selectedDates?.[0], dateOnly))
+    })
     vpRef.current = newVp
     return () => {
       // destroy the flatpickr instance when the dom element is removed
@@ -54,26 +58,14 @@ export default function useFlatpickr(
         newVp.destroy()
       }
     }
-  }, [flatpickrGuid, fpOpts, htmlElement, id, vpRef])
-
-  React.useEffect(() => {
-    if (vpRef.current && vpRef.current.config) {
-      vpRef.current.set('onChange', (selectedDates: Date[]) => {
-        onChange(getDateValue(selectedDates[0]))
-      })
-    }
-  }, [getDateValue, onChange, vpRef])
+  }, [dateOnly, flatpickrGuid, fpOpts, htmlElement, id, onChange])
 
   // Sync value with flatpickr when value is changed outside of component
   React.useEffect(() => {
-    // try {
     const vp = vpRef.current
 
     if (vp && vp.selectedDates) {
       const selectedDate = vp.selectedDates[0]
-      // Sentry.captureException(
-      //   new Error(`selectedDate: ${selectedDate} value: ${value}`),
-      // )
       if (!value && selectedDate) {
         try {
           vp.clear(false)
@@ -83,18 +75,14 @@ export default function useFlatpickr(
       } else if (
         value &&
         typeof value === 'string' &&
-        (!selectedDate || getDateValue(selectedDate) !== value)
+        (!selectedDate || getDateValue(vp, selectedDate, dateOnly) !== value)
       ) {
         try {
-          vp.setDate(value, false)
-          // vp.setDate('2021-10-07', false)
+          vp.setDate(new Date(value), false)
         } catch (error) {
           Sentry.captureException(new Error(`Error setting date: ${value}`))
         }
       }
     }
-    // } catch (error) {
-    //   Sentry.captureException(error)
-    // }
-  }, [getDateValue, value, vpRef])
+  }, [dateOnly, value])
 }
