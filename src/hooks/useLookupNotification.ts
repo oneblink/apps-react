c45fbca9c2fd0f4b3b6c388c70ea368f51974cbc
Matt Carroll <matt.carroll139@gmail.com>
2023-06-19 20:02:50 +1000

    ON-32622 # Changed lookups to stop when input value changes

--- src/hooks/useLookupNotification.ts
+++ src/hooks/useLookupNotification.ts
@@ -1,9 +1,13 @@
 import * as React from 'react'
 
-type LookupNotificationContextValue = {
+export type LookupNotificationContextValue = {
   isLookup: boolean
   isDisabled: boolean
-  onLookup: (value: unknown, abortSignal: AbortSignal) => Promise<void>
+  onLookup: (options: {
+    newValue: unknown
+    abortController: AbortController
+    continueLookupOnAbort: boolean
+  }) => Promise<void>
 }
 
 const defaultContext = {
@@ -24,21 +28,36 @@ export default function useLookupNotification(value: unknown) {
   // and have it aborted if the user clicks the lookup button again.
   const [lookupCount, setLookupCount] = React.useState(0)
 
+  // We use a ref here to keep the abort controller outside of the useEffect
+  // below. This way we can abort the lookup if the value changes while the
+  // lookup is running.
+  const abortControllerRef = React.useRef<AbortController | null>(null)
+
   React.useEffect(() => {
     if (lookupCount === 0) {
       return
     }
-    const abortController = new AbortController()
-    onLookup(value, abortController.signal)
-    return () => {
-      abortController.abort()
-    }
+    abortControllerRef.current = new AbortController()
+    onLookup({
+      newValue: value,
+      abortController: abortControllerRef.current,
+      continueLookupOnAbort: false,
+    })
     // Wants to use "onLookup" and "value" as dependencies,
     // however, these will change on any change made on any
     // element. Checking if "lookupCount" has changed is enough
     // to trigger a lookup when the correct dependencies change
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [lookupCount])
+
+  React.useEffect(() => {
+    const abortController = abortControllerRef.current
+    if (abortController) {
+      return () => {
+        abortController.abort()
+      }
+    }
+  }, [value, lookupCount])
 
   return {
     ...state,
