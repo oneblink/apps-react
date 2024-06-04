efe24a59a29e163a7051a42679c1d9d4995dc1de
Matt Carroll <matt.carroll139@gmail.com>
2024-06-04 11:22:25 +1000

    ON-40912 # Changed @arcgis/core libraries to load from CDN via ES Modules

---
+++ src/types/arcgis.d.ts
@@ -0,0 +1,32 @@
+declare module 'https://js.arcgis.com/4.29/@arcgis/core/views/MapView.js' {
+  import MapView from '@arcgis/core/views/MapView'
+  export default MapView
+}
+declare module 'https://js.arcgis.com/4.29/@arcgis/core/WebMap.js' {
+  import WebMap from '@arcgis/core/WebMap'
+  export default WebMap
+}
+declare module 'https://js.arcgis.com/4.29/@arcgis/core/widgets/Home.js' {
+  import Home from '@arcgis/core/widgets/Home.js'
+  export default Home
+}
+declare module 'https://js.arcgis.com/4.29/@arcgis/core/widgets/Search.js' {
+  import Search from '@arcgis/core/widgets/Search.js'
+  export default Search
+}
+declare module 'https://js.arcgis.com/4.29/@arcgis/core/widgets/Zoom.js' {
+  import Zoom from '@arcgis/core/widgets/Zoom.js'
+  export default Zoom
+}
+declare module 'https://js.arcgis.com/4.29/@arcgis/core/widgets/LayerList.js' {
+  import LayerList from '@arcgis/core/widgets/LayerList.js'
+  export default LayerList
+}
+declare module 'https://js.arcgis.com/4.29/@arcgis/core/widgets/Expand.js' {
+  import Expand from '@arcgis/core/widgets/Expand.js'
+  export default Expand
+}
+declare module 'https://js.arcgis.com/4.29/@arcgis/core/widgets/BasemapGallery.js' {
+  import BaseMapGallery from '@arcgis/core/widgets/BasemapGallery.js'
+  export default BaseMapGallery
+}
