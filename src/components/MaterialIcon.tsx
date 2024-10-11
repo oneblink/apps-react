338f218676dfcbab81d045cc9aab8408a78860c8
Zac Turner <zac@oneblink.io>
2024-10-11 13:18:14 +1100

    ON-44140 # Fixed validation errors card overlapping pager on tiles apps; Made scrolling go to element label; Used bulma vars for some css values; Fixed some react key and ref console errors.

--- src/components/MaterialIcon.tsx
+++ src/components/MaterialIcon.tsx
@@ -2,16 +2,17 @@ import * as React from 'react'
 import { Icon } from '@mui/material'
 import clsx from 'clsx'
 
-export default function MaterialIcon({
-  className,
-  ...props
-}: React.ComponentProps<typeof Icon>) {
+export default React.forwardRef<
+  HTMLSpanElement,
+  React.ComponentProps<typeof Icon>
+>(function MaterialIcon({ className, ...props }, ref) {
   return (
     <Icon
+      ref={ref}
       role="img"
       className={clsx('ob-icon', className)}
       aria-hidden
       {...props}
     />
   )
-}
+})
