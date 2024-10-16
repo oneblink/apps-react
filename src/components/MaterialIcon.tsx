ad7c1848a704e85d2e2f6484d7cd80572899ac9a
Zac Turner <zac@oneblink.io>
2024-10-16 11:15:17 +1100

    Change to type declaration to assuage a type check error in forms-cdn

--- src/components/MaterialIcon.tsx
+++ src/components/MaterialIcon.tsx
@@ -5,7 +5,13 @@ import clsx from 'clsx'
 export default React.forwardRef<
   HTMLSpanElement,
   React.ComponentProps<typeof Icon>
->(function MaterialIcon({ className, ...props }, ref) {
+>(function MaterialIcon(
+  // We declare the prop types again here because it
+  // changes the way the type dec file is generated,
+  // mitigating a type check error in the forms-cdn repo.
+  { className, ...props }: React.ComponentProps<typeof Icon>,
+  ref,
+) {
   return (
     <Icon
       ref={ref}
