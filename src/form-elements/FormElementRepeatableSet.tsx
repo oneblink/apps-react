39a9605374595cbe737dac1e6632e6db65dc3545
Matt Carroll <matt.carroll139@gmail.com>
2025-01-29 10:37:57 +1100

    ON-46457 # Added collapse animation for repeatable set entries

--- src/form-elements/FormElementRepeatableSet.tsx
+++ src/form-elements/FormElementRepeatableSet.tsx
@@ -2,7 +2,9 @@ import * as React from 'react'
 import clsx from 'clsx'
 import { FormTypes, SubmissionTypes } from '@oneblink/types'
 import useBooleanState from '../hooks/useBooleanState'
-import generateDefaultData from '../services/generate-default-data'
+import generateDefaultData, {
+  ENTRY_ID_PROPERTY_NAME,
+} from '../services/generate-default-data'
 import OneBlinkFormElements from '../components/renderer/OneBlinkFormElements'
 import Modal from '../components/renderer/Modal'
 import FormElementLabelContainer from '../components/renderer/FormElementLabelContainer'
@@ -22,6 +24,7 @@ import useFormElementRepeatableSetEntries from '../hooks/useFormElementRepeatabl
 import useElementAriaDescribedby from '../hooks/useElementAriaDescribedby'
 import MaterialIcon from '../components/MaterialIcon'
 import ElementDOMId from '../utils/elementDOMIds'
+import { Collapse } from '@mui/material'
 
 type Props = {
   formId: number
@@ -40,6 +43,57 @@ type Props = {
 } & IsDirtyProps
 
 const RepeatableSetIndexContext = React.createContext<number>(0)
+
+function RepeatableSetEntryProvider({
+  index,
+  isAnimated,
+  onRemove,
+  children,
+}: {
+  isAnimated: boolean
+  index: number
+  onRemove: (index: number) => void
+  children: (renderProps: { onRemove: () => void }) => React.ReactNode
+}) {
+  const [isExpanded, setIsExpanded] = React.useState(true)
+
+  const handleRemove = React.useCallback(() => {
+    if (isAnimated) {
+      setIsExpanded(false)
+    } else {
+      onRemove(index)
+    }
+  }, [index, isAnimated, onRemove])
+
+  const node = (
+    <RepeatableSetIndexContext.Provider value={index}>
+      {children({
+        onRemove: handleRemove,
+      })}
+    </RepeatableSetIndexContext.Provider>
+  )
+
+  if (!isAnimated) {
+    return node
+  }
+
+  return (
+    <Collapse
+      in={isExpanded}
+      appear
+      onExited={() => {
+        onRemove(index)
+      }}
+      classes={{
+        root: 'ob-repeatable-set__collapsible',
+        wrapper: 'ob-repeatable-set__collapsible-wrapper',
+        wrapperInner: 'ob-repeatable-set__collapsible-wrapper-inner',
+      }}
+    >
+      {node}
+    </Collapse>
+  )
+}
 
 export function useRepeatableSetIndexText(text: string) {
   const index = React.useContext(RepeatableSetIndexContext)
@@ -250,7 +304,7 @@ function FormElementRepeatableSet({
         {entries.map((entry, index) => {
           return (
             <RepeatableSetEntry
-              key={index}
+              key={entry[ENTRY_ID_PROPERTY_NAME] as string}
               formId={formId}
               index={index}
               id={id}
@@ -460,88 +514,96 @@ const RepeatableSetEntry = React.memo<RepeatableSetEntryProps>(
     const elementDOMId = React.useMemo(() => new ElementDOMId(id), [id])
 
     return (
-      <RepeatableSetIndexContext.Provider value={index}>
-        <Modal
-          isOpen={isConfirmingRemove}
-          className="cypress-repeatable-set-prompt"
-          titleClassName="cypress-repeatable-set-remove-entry-header"
-          title={element.removeSetEntryLabel || 'Remove Entry'}
-          actions={
-            <>
-              <button
-                type="button"
-                className="button ob-button is-light cypress-cancel-repeatable-set"
-                onClick={cancelRemove}
-              >
-                Cancel
-              </button>
-              <button
-                type="button"
-                className="button ob-button is-primary cypress-confirm-repeatable-set"
-                onClick={() => {
-                  cancelRemove()
-                  onRemove(index)
-                }}
-                autoFocus
-              >
-                Yes
-              </button>
-            </>
-          }
-        >
-          Are you sure you want to remove this entry?
-        </Modal>
+      <RepeatableSetEntryProvider
+        index={index}
+        onRemove={onRemove}
+        isAnimated={element.layout === 'MULTIPLE_ADD_BUTTONS'}
+      >
+        {({ onRemove }) => (
+          <>
+            <Modal
+              isOpen={isConfirmingRemove}
+              className="cypress-repeatable-set-prompt"
+              titleClassName="cypress-repeatable-set-remove-entry-header"
+              title={element.removeSetEntryLabel || 'Remove Entry'}
+              actions={
+                <>
+                  <button
+                    type="button"
+                    className="button ob-button is-light cypress-cancel-repeatable-set"
+                    onClick={cancelRemove}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="button"
+                    className="button ob-button is-primary cypress-confirm-repeatable-set"
+                    onClick={() => {
+                      cancelRemove()
+                      onRemove()
+                    }}
+                    autoFocus
+                  >
+                    Yes
+                  </button>
+                </>
+              }
+            >
+              Are you sure you want to remove this entry?
+            </Modal>
 
-        <div
-          key={index}
-          className={clsx(
-            'ob-repeatable-set__container cypress-repeatable-set-container',
-            validationClassName,
-          )}
-        >
-          {(!element.layout || element.layout === 'SINGLE_ADD_BUTTON') && (
-            <RemoveButton
-              onConfirmRemove={confirmRemove}
-              element={element}
-              classes={['is-pulled-right']}
-              index={index}
-            />
-          )}
+            <div
+              className={clsx(
+                'ob-repeatable-set__container cypress-repeatable-set-container',
+                validationClassName,
+              )}
+            >
+              {(!element.layout || element.layout === 'SINGLE_ADD_BUTTON') && (
+                <RemoveButton
+                  onConfirmRemove={confirmRemove}
+                  element={element}
+                  className="ob-repeatable-set__button-remove-top"
+                  index={index}
+                />
+              )}
 
-          <OneBlinkFormElements
-            formId={formId}
-            idPrefix={elementDOMId.repeatableSetEntryDOMIdPrefix(
-              index.toString(),
-            )}
-            isEven={isEven}
-            formElementsValidation={formElementsValidation}
-            displayValidationMessages={displayValidationMessages}
-            elements={element.elements}
-            onChange={handleChange}
-            onLookup={handleLookup}
-            model={entry}
-            parentElement={element}
-            formElementsConditionallyShown={formElementsConditionallyShown}
-            onUpdateFormElements={handleUpdateNestedFormElements}
-          />
-          {element.layout === 'MULTIPLE_ADD_BUTTONS' && (
-            <div className="is-flex">
-              <RemoveButton
-                onConfirmRemove={confirmRemove}
-                element={element}
-                index={index}
+              <OneBlinkFormElements
+                formId={formId}
+                idPrefix={elementDOMId.repeatableSetEntryDOMIdPrefix(
+                  index.toString(),
+                )}
+                isEven={isEven}
+                formElementsValidation={formElementsValidation}
+                displayValidationMessages={displayValidationMessages}
+                elements={element.elements}
+                onChange={handleChange}
+                onLookup={handleLookup}
+                model={entry}
+                parentElement={element}
+                formElementsConditionallyShown={formElementsConditionallyShown}
+                onUpdateFormElements={handleUpdateNestedFormElements}
               />
+              {element.layout === 'MULTIPLE_ADD_BUTTONS' && (
+                <RemoveButton
+                  onConfirmRemove={confirmRemove}
+                  element={element}
+                  index={index}
+                  className="ob-repeatable-set__button-remove-bottom"
+                />
+              )}
             </div>
-          )}
-        </div>
-        {showAddButton && (
-          <AddButton
-            onAdd={() => onAdd(index + 1)}
-            element={element}
-            classes={['ob-button-repeatable-set-layout__multiple-add-buttons']}
-          />
+            {showAddButton && (
+              <AddButton
+                onAdd={() => onAdd(index + 1)}
+                element={element}
+                classes={[
+                  'ob-button-repeatable-set-layout__multiple-add-buttons',
+                ]}
+              />
+            )}
+          </>
         )}
-      </RepeatableSetIndexContext.Provider>
+      </RepeatableSetEntryProvider>
     )
   },
 )
@@ -578,20 +640,20 @@ function AddButton({
 function RemoveButton({
   onConfirmRemove,
   element,
-  classes,
+  className,
   index,
 }: {
   onConfirmRemove: () => void
   element: FormTypes.RepeatableSetElement
-  classes?: string[]
+  className?: string
   index: number
 }) {
   return (
     <button
       type="button"
       className={clsx(
-        'button ob-button_remove is-light cypress-remove-repeatable-set-entry',
-        ...(classes ?? []),
+        'button ob-button ob-button_remove is-light cypress-remove-repeatable-set-entry',
+        className,
       )}
       onClick={onConfirmRemove}
       disabled={element.readOnly}
