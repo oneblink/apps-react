a61c0c5e39e695bfa7bbd82a428d71de4ab152ef
Ryan Button <ryan@oneblink.io>
2025-05-15 11:13:17 +1000

    MS-177 # Push error to sentry

--- src/utils/sendGAEvents.ts
+++ src/utils/sendGAEvents.ts
@@ -1,3 +1,5 @@
+import { Sentry } from "@oneblink/apps"
+
 export const sendGoogleAnalyticsEvent = (
   eventName: string,
   eventParams: Record<string, unknown>,
@@ -8,6 +10,7 @@ export const sendGoogleAnalyticsEvent = (
       window.gtag('event', eventName, eventParams)
     } catch (e) {
       console.warn('An error occured pushing event to google analytics: ', e)
+      Sentry.captureException(e)
     }
   }
 }
