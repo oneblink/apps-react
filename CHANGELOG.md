ae6e4c3798032d1ededf3b3763d86e73510bcccc
kizaonline <kizaonline@gmail.com>
2025-05-27 11:02:19 +1000

    MS-248 # Add `displayAsCheckbox` support for Switch element

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - `requiresConfirmation` support for email form elements
+- `displayAsCheckbox` support for Switch element
 
 ## [8.3.0] - 2025-05-22
 
--- src/form-elements/FormElementBoolean.tsx
+++ src/form-elements/FormElementBoolean.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { FormTypes } from '@oneblink/types'
 import FormElementLabelContainer from '../components/renderer/FormElementLabelContainer'
-import { Switch } from '@mui/material'
+import { Checkbox, Switch } from '@mui/material'
 import { FormElementValueChangeHandler, IsDirtyProps } from '../types/form'
 import { LookupNotificationContext } from '../hooks/useLookupNotification'
 import useElementAriaDescribedby from '../hooks/useElementAriaDescribedby'
@@ -38,26 +38,50 @@ function FormElementBoolean({
         element={element}
         required={element.required}
         leading={
-          <Switch
-            id={id}
-            name={element.name}
-            color="primary"
-            edge="start"
-            className="ob-boolean__input cypress-boolean-control"
-            classes={{
-              checked: 'ob-boolean__input-checked',
-            }}
-            checked={!!value}
-            disabled={element.readOnly}
-            onChange={(e) => {
-              setIsDirty()
-              onChange(element, {
-                value: e.target.checked,
-              })
-            }}
-            aria-describedby={ariaDescribedby}
-            onBlur={setIsDirty}
-          />
+          element.displayAsCheckbox ? (
+            <Checkbox
+              color="primary"
+              classes={{
+                checked: 'ob-boolean-checkbox__input-checked',
+              }}
+              className="ob-boolean-checkbox__input cypress-checkbox-control"
+              id={id}
+              checked={!!value}
+              onChange={(e) => {
+                setIsDirty()
+                onChange(element, {
+                  value: e.target.checked,
+                })
+              }}
+              disabled={element.readOnly}
+              edge="start"
+              inputProps={{
+                'aria-describedby': ariaDescribedby,
+              }}
+              onBlur={setIsDirty}
+            />
+          ) : (
+            <Switch
+              id={id}
+              name={element.name}
+              color="primary"
+              edge="start"
+              className="ob-boolean__input cypress-boolean-control"
+              classes={{
+                checked: 'ob-boolean__input-checked',
+              }}
+              checked={!!value}
+              disabled={element.readOnly}
+              onChange={(e) => {
+                setIsDirty()
+                onChange(element, {
+                  value: e.target.checked,
+                })
+              }}
+              aria-describedby={ariaDescribedby}
+              onBlur={setIsDirty}
+            />
+          )
         }
       >
         {isDisplayingValidationMessage && (
