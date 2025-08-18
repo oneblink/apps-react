0daa6c64e52006faec4cb2a88665fa81f7a07bfd
Zac Turner <zac@oneblink.io>
2025-08-18 14:23:10 +1000

    ap-4568 # Added component docs

--- src/components/downloadable-files/index.tsx
+++ src/components/downloadable-files/index.tsx
@@ -7,17 +7,22 @@ import {
 } from './resource-components'
 import { LayoutProvider, LayoutType } from './LayoutProvider'
 
-type Props = {
-  formSubmissionResult: submissionService.FormSubmissionResult
-  divider?: boolean
-  layout?: LayoutType
-}
-
-const DownloadableFiles = ({
+function DownloadableFiles({
   formSubmissionResult,
   divider,
   layout,
-}: Props) => {
+}: {
+  /** The form submission with the pdf and/or attachments config on it. */
+  formSubmissionResult: submissionService.FormSubmissionResult
+  /** Whether to render a divider above the content. */
+  divider?: boolean
+  /**
+   * The layout to use for the downloadable files. Options are `GRID` and
+   * `LIST`. Defaults to `GRID`. `GRID` will render files next to each other at
+   * some screen sizes. `LIST` will always render each file on a new line.
+   */
+  layout?: LayoutType
+}) {
   const pdfFileNode = React.useMemo(
     () =>
       !formSubmissionResult.downloadSubmissionPdfUrl ? undefined : (
@@ -59,4 +64,60 @@ const DownloadableFiles = ({
   )
 }
 
-export default React.memo<Props>(DownloadableFiles)
+/**
+ * Component for rendering post-submission downloadable files such as PDFs and
+ * attachments. The only thing required is the `formSubmissionResult`.
+ *
+ * It is also recommended to import the `css` from this library as well.
+ *
+ * ```js
+ * import { DownloadableFiles } from '@oneblink/apps-react'
+ * import '@oneblink/apps-react/dist/styles.css'
+ * ```
+ *
+ * #### Example
+ *
+ * ```tsx
+ * import React from 'react'
+ * import ReactDOM from 'react-dom'
+ * import { DownloadableFiles } from '@oneblink/apps-react'
+ * import '@oneblink/apps-react/dist/styles.css'
+ *
+ * function SubmissionContainer() {
+ *
+ *   const [state, setState] = useState(null)
+ *
+ *   const handleSubmit = React.useCallback(async () => {
+ *     ...
+ *     setState(result)
+ *   }, [])
+ *
+ *   return (
+ *     <div>
+ *      ...
+ *       {state && (
+ *         <DownloadableFiles formSubmissionResult={state} />
+ *       )}
+ *     </div>
+ *   )
+ * }
+ *
+ * function App() {
+ *   return (
+ *     <IsOfflineContextProvider>
+ *       <SubmissionContainer />
+ *     </IsOfflineContextProvider>
+ *   )
+ * }
+ *
+ * const root = document.getElementById('root')
+ * if (root) {
+ *   ReactDOM.render(<App />, root)
+ * }
+ * ```
+ *
+ * @param props
+ * @returns
+ * @group Components
+ */
+export default React.memo(DownloadableFiles)
