cab9ffbb65611c621de6e5c8356204226dbfe961
Matt Carroll <matt.carroll139@gmail.com>
2025-11-17 10:23:08 +1100

    AP-5992 # Fixed legacy quill CSS breaking current quill lists

--- src/components/QuillHTML.tsx
+++ src/components/QuillHTML.tsx
@@ -8,11 +8,18 @@ export default function QuillHTML({
 }: React.ComponentProps<'div'> & {
   html: string
 }) {
-  const __html = React.useMemo(() => sanitizeHtml(html), [html])
+  const [__html, includesLegacyLists] = React.useMemo(() => {
+    const sanitizedHtml = sanitizeHtml(html)
+    return [sanitizedHtml, sanitizedHtml.includes('ql-indent-')]
+  }, [html])
   return (
     <div
       {...props}
-      className={clsx(props.className, 'ob-quill-content')}
+      className={clsx(props.className, 'ob-quill-content', {
+        'ob-quill-legacy-content': includesLegacyLists,
+        // "content" is a bulma class: https://bulma.io/documentation/elements/content/
+        content: !includesLegacyLists,
+      })}
       // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{
         __html,
