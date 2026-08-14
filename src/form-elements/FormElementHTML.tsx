58b383f3c0a3a08a1114b2f1972a9c5a4c8d39db
Kiza Online <kizaonline@gmail.com>
2026-08-14 11:17:05 +1000

    AP-9963 # Move to outer div

--- src/form-elements/FormElementHTML.tsx
+++ src/form-elements/FormElementHTML.tsx
@@ -12,11 +12,13 @@ function FormElementHTML({ element }: Props) {
 
   return (
     <div className="cypress-html-element">
-      <div className="ob-form__element ob-information cypress-html-element">
+      <div
+        className="ob-form__element ob-information cypress-html-element"
+        role={element.screenReaderAlert ? 'alert' : undefined}
+      >
         <QuillHTML
           html={html}
           className="cypress-html-element-content ob-information__content"
-          role={element.screenReaderAlert ? 'alert' : undefined}
         />
       </div>
     </div>
